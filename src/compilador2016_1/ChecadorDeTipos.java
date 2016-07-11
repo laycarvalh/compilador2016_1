9324f773895f44294bd54cb4cf58b0903aba6b32
Layse Carvalho <lay.carvalh@gmail.com>
2016-07-11 17:49:07 -0300

    Teste de commit

--- src/compilador2016_1/ChecadorDeTipos.java
+++ src/compilador2016_1/ChecadorDeTipos.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package compilador2016_1;
 
 import java.util.HashMap;
